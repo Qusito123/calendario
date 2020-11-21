eb0b36b363185b46517c45102a51a66862fc695c
Oscar Dominguez <oscardominguezn.n@outlook.com>
2020-11-21 16:16:23 -0600

    Terminado 1.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,31 @@
 import 'dart:convert';
-import 'dart:ffi';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-
-void main(){
+void main() {
   initializeDateFormatting().then((_) => runApp(MyApp()));
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Calendario',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: HomePage(),
       debugShowCheckedModeBanner: false,
-      );
+    );
   }
 }
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>{
+class _HomePageState extends State<HomePage> {
   CalendarController _controller;
   Map<DateTime, List<dynamic>> _events;
   TextEditingController _eventController;
@@ -73,108 +71,148 @@ class _HomePageState extends State<HomePage>{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('Calendario'),)
-      ),
+          title: Center(
+        child: Text('Calendario'),
+      )),
       body: Container(
         decoration: BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage(
-                    "https://images.unsplash.com/photo-1603657994296-a7134fd1e33b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80"
-                    ),
-            fit: BoxFit.cover
-        )
-        ),
+            image: DecorationImage(
+                image: AssetImage('assets/image/wp.jpeg'), fit: BoxFit.cover)),
         child: SingleChildScrollView(
           child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TableCalendar(
-              locale: 'es_Es',
-              events: _events,
-              initialCalendarFormat: CalendarFormat.month,
-              startingDayOfWeek: StartingDayOfWeek.monday,
-              calendarStyle: CalendarStyle(
-                outsideDaysVisible: false,
-                todayColor: Colors.orange, 
-                selectedColor: Theme.of(context).primaryColor,
-                todayStyle: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18.0,
-                  color: Colors.white
-                  ),
-                weekendStyle: TextStyle(
-                    color: Colors.green
-                  ),
-                weekdayStyle: TextStyle(
-                    color: Colors.blue
-                  ),
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TableCalendar(
+                locale: 'es_Es',
+                events: _events,
+                initialCalendarFormat: CalendarFormat.month,
+                startingDayOfWeek: StartingDayOfWeek.monday,
+                calendarStyle: CalendarStyle(
+                  outsideDaysVisible: false,
+                  todayColor: Colors.orange,
+                  selectedColor: Theme.of(context).primaryColor,
+                  todayStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0,
+                      color: Colors.white),
+                  weekendStyle: TextStyle(color: Colors.green),
+                  weekdayStyle: TextStyle(color: Colors.blue),
                 ),
-              daysOfWeekStyle: DaysOfWeekStyle(
-                weekdayStyle: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold,), 
-                weekendStyle: TextStyle(color: Colors.green, fontWeight: FontWeight.bold)
-              ),
-              headerStyle: HeaderStyle(
-                centerHeaderTitle: true,
-                formatButtonDecoration: BoxDecoration(
-                  color: Colors.orange,
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                formatButtonTextStyle: TextStyle(
-                  color: Colors.white
-                ),
-                formatButtonShowsNext: false,
-              ),
-              onDaySelected: (DateTime day, List events, List holidays) {
-                setState(() {
-                  _selectedEvents = events;
-                });
-              },
-              builders: CalendarBuilders(
-                selectedDayBuilder: (context, date, events) => Container(
-                  margin: const EdgeInsets.all(4.0),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(10.0)
+                daysOfWeekStyle: DaysOfWeekStyle(
+                    weekdayStyle: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    weekendStyle: TextStyle(
+                        color: Colors.green, fontWeight: FontWeight.bold)),
+                headerStyle: HeaderStyle(
+                  centerHeaderTitle: true,
+                  titleTextStyle:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+                  decoration: BoxDecoration(color: Colors.white54),
+                  formatButtonDecoration: BoxDecoration(
+                    color: Colors.orange,
+                    borderRadius: BorderRadius.circular(20.0),
                   ),
-                  child: Text(date.day.toString(), style: TextStyle(color: Colors.white),),
+                  formatButtonTextStyle: TextStyle(color: Colors.white),
+                  formatButtonShowsNext: false,
+                ),
+                onDaySelected: (DateTime day, List events, List holidays) {
+                  setState(() {
+                    _selectedEvents = events;
+                  });
+                },
+                builders: CalendarBuilders(
+                  selectedDayBuilder: (context, date, events) => Container(
+                    margin: const EdgeInsets.all(4.0),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Text(
+                      date.day.toString(),
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
                   todayDayBuilder: (context, date, events) => Container(
-                  margin: const EdgeInsets.all(4.0),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.orange,
-                    borderRadius: BorderRadius.circular(10.0)
-                  ),
-                  child: Text(date.day.toString(), style: TextStyle(color: Colors.white),),
+                    margin: const EdgeInsets.all(4.0),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Text(
+                      date.day.toString(),
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
                   dayBuilder: (context, date, events) => Container(
-                  margin: const EdgeInsets.all(4.0),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.white54,
-                    borderRadius: BorderRadius.circular(10.0)
-                  ),
-                  child: Text(date.day.toString(), style: TextStyle(color: Colors.black, fontWeight: FontWeight.w800),),
+                    margin: const EdgeInsets.all(4.0),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.white54,
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Text(
+                      date.day.toString(),
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w800,
+                          fontSize: 18.0),
+                    ),
                   ),
                   weekendDayBuilder: (context, date, events) => Container(
-                  margin: const EdgeInsets.all(4.0),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(10.0)
+                    margin: const EdgeInsets.all(4.0),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Text(
+                      date.day.toString(),
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w800,
+                          fontSize: 18.0),
+                    ),
                   ),
-                  child: Text(date.day.toString(), style: TextStyle(color: Colors.black, fontWeight: FontWeight.w800),),
+                  dowWeekdayBuilder: (context, weekday) => Container(
+                    margin: const EdgeInsets.all(4.0),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.white54,
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Text(
+                      weekday.toString(),
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w800,
+                          fontSize: 15.0),
+                    ),
                   ),
+                  dowWeekendBuilder: (context, weekday) => Container(
+                    margin: const EdgeInsets.all(4.0),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Text(
+                      weekday.toString(),
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w800,
+                          fontSize: 15.0),
+                    ),
+                  ),
+                ),
+                calendarController: _controller,
               ),
-              calendarController: _controller,
-              ),
-              ... _selectedEvents.map((event) => ListTile(
-                title: Text(event),
-              ))
-          ],
+              ..._selectedEvents.map((event) => ListTile(
+                    title: Text(event,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20.0)),
+                    tileColor: Theme.of(context).canvasColor,
+                  ))
+            ],
           ),
-      ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
@@ -183,28 +221,30 @@ class _HomePageState extends State<HomePage>{
     );
   }
 
-  _showAddDialog(){
+  _showAddDialog() {
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
-        title: Center(child: Text('Agregar evento'),),
+        title: Center(
+          child: Text('Agregar evento'),
+        ),
         content: TextField(
           controller: _eventController,
         ),
         actions: [
           FlatButton(
-            onPressed: (){
-              if(_eventController.text.isEmpty) return;
-              if(_events[_controller.selectedDay] != null){
-                _events[_controller.selectedDay].add(_eventController.text);
-              }else{
-                _events[_controller.selectedDay] = [_eventController.text];
-              }
-              prefs.setString("events", json.encode(encodeMap(_events)));
-              _eventController.clear();
-              Navigator.pop(context);
-          }, 
-          child: Text("Guardar"))
+              onPressed: () {
+                if (_eventController.text.isEmpty) return;
+                if (_events[_controller.selectedDay] != null) {
+                  _events[_controller.selectedDay].add(_eventController.text);
+                } else {
+                  _events[_controller.selectedDay] = [_eventController.text];
+                }
+                prefs.setString("events", json.encode(encodeMap(_events)));
+                _eventController.clear();
+                Navigator.pop(context);
+              },
+              child: Text("Guardar"))
         ],
       ),
     );
